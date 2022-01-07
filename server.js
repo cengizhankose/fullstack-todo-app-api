8c5ab18ade9ddbe77b8bbff159fa50af6365c77b
Cengizhan KOSE <kose651@gmail.com>
2022-01-07 14:26:05 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const dotenv = require("dotenv");
+var cors = require("cors");
+const colors = require("colors");
+const { json } = require("body-parser");
+const { nanoid } = require("nanoid");
+
+dotenv.config({ path: "./config.env" });
+
+var app = express();
+
+app.use(cors());
+app.use(json());
+
+let todos = [
+  {
+    id: nanoid(),
+    title: "todo 1",
+    completed: true,
+  },
+  {
+    id: nanoid(),
+    title: "todo 2",
+    completed: false,
+  },
+  {
+    id: nanoid(),
+    title: "todo 3",
+    completed: true,
+  },
+  {
+    id: nanoid(),
+    title: "todo 4",
+    completed: false,
+  },
+  {
+    id: nanoid(),
+    title: "todo 5",
+    completed: false,
+  },
+];
+
+app.get("/", (req, res) => res.send("Welcome to todo-api"));
+
+app.get("/todos", (req, res) => res.send(todos));
+
+app.post("/todos", (req, res) => {
+  const title = req.body.title;
+  switch (title) {
+    case null:
+      res.statusMessage = "title value cannot be null";
+      res.status(400).end();
+      break;
+    case undefined:
+      res.statusMessage = "title value cannot be undefined";
+      res.status(400).end();
+      break;
+    case "":
+      res.statusMessage = "title value cannot be empty string";
+      res.status(400).end();
+      break;
+    default:
+      const todo = { id: nanoid(), title, completed: false };
+      todos.push(todo);
+      return res.send(todo);
+      break;
+  }
+});
+
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const index = todos.findIndex((todo) => todo?.id === id);
+  const completed = Boolean(req.body?.completed);
+  if (index > -1) {
+    todos[index].completed = completed;
+  }
+  return res.send(todos[index]);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+  res.send(todos);
+});
+
+const PORT = 7001;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`.green.bold));
+
+module.exports = app;
---
+++ server.test.js
@@ -0,0 +1,116 @@
+const request = require("supertest");
+const server = require("./server");
+
+describe("GET /todos", () => {
+  test("should respond with a 200 status code", async () => {
+    const response = await request(server).get("/todos");
+    expect(response.statusCode).toBe(200);
+  });
+  test("should respond with an array of todos", async () => {
+    const response = await request(server).get("/todos");
+    expect(response).toBeDefined();
+  });
+});
+
+describe("POST /todos", () => {
+  describe("given a title for todo", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(server)
+        .post("/todos")
+        .send({ id: 0, title: "todooo", completed: false });
+      expect(response.statusCode).toBe(200);
+    });
+    test("should specify json in the content type header", async () => {
+      const response = await request(server)
+        .post("/todos")
+        .send({ id: 0, title: "todooo", completed: false });
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+    test("response has a body that contains todo datas", async () => {
+      const response = await request(server)
+        .post("/todos")
+        .send({ id: 0, title: "todooo", completed: false });
+      expect(response.body.id).toBeDefined();
+      expect(response.body.title).toBeDefined();
+      expect(response.body.completed).toBeDefined();
+    });
+  });
+  describe("when the title is invalid", () => {
+    test("should respond with a status code of 400", async () => {
+      const bodyData = [
+        { title: null },
+        { title: undefined },
+        { title: "" },
+        {},
+      ];
+      for (const body of bodyData) {
+        const response = await request(server).post("/todos").send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+  });
+});
+
+describe("DELETE /todos", () => {
+  describe("given a id for todo", () => {
+    test("should respond with a 200 status code", async () => {
+      const before = await request(server).get("/todos");
+      const response = await request(server).delete(
+        `/todos/${before.body[before.body.length - 1].id}`
+      );
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("response has a body that contains todo datas", async () => {
+      const before = await request(server).get("/todos");
+      const response = await request(server).delete(
+        `/todos/${before.body[before.body.length - 1].id}`
+      );
+      expect(response.body.length).toBeGreaterThanOrEqual(0);
+    });
+
+    test("deletes a todo", async () => {
+      const before = await request(server).get("/todos");
+      const response = await request(server).delete(
+        `/todos/${before.body[before.body.length - 1].id}`
+      );
+      const after = await request(server).get("/todos");
+
+      expect(before.body.length).toBeGreaterThan(after.body.length);
+    });
+  });
+});
+
+describe("PATCH /todos", () => {
+  describe("given a id for todo", () => {
+    test("should respond with a 200 status code", async () => {
+      const before = await request(server).get("/todos");
+      const response = await request(server).patch(
+        `/todos/${before.body[before.body.length - 1].id}`
+      );
+      expect(response.statusCode).toBe(200);
+    });
+
+    test("response has a body that contains todo datas", async () => {
+      const before = await request(server).get("/todos");
+      const response = await request(server).patch(
+        `/todos/${before.body[before.body.length - 1].id}`
+      );
+      expect(response.body).toBeDefined();
+    });
+
+    test("updates the todo", async () => {
+      const before = await request(server).get("/todos");
+      await request(server)
+        .patch(`/todos/${before.body[before.body.length - 1].id}`)
+        .send({ completed: !before.body[before.body.length - 1].completed });
+      const after = await request(server).get("/todos");
+
+      expect(before.body[before.body.length - 1].completed).toBe(
+        !after.body[after.body.length - 1].completed
+      );
+    });
+  });
+});
